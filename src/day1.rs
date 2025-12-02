e0cf33e8ed594db93572d85c56b43b93668857e7
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2025-12-01 21:22:45 -0600

    solve day1p2

--- src/day1.rs
+++ src/day1.rs
@@ -16,7 +16,6 @@ pub fn part1(input: &str) -> u32 {
             rots * mul
         })
         .for_each(|rot| {
-            println!("dial: {dial}");
             dial += rot;
             while dial < 0 {
                 dial += 100
@@ -31,12 +30,74 @@ pub fn part1(input: &str) -> u32 {
     count
 }
 
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> u32 {
+    let mut dial = 50;
+    let mut count = 0;
+    input
+        .lines()
+        .map(|l| {
+            let mul = match l.as_bytes().first().unwrap() {
+                b'L' => -1,
+                b'R' => 1,
+                _ => panic!("non lr"),
+            };
+            let rots: i32 = l[1..].parse().unwrap();
+            rots * mul
+        })
+        .for_each(|mut rot| {
+            // dial += rot;
+            // while dial > 100 {
+            //     dial -= 100;
+            //     count += 1;
+            // }
+            // while dial < 0 {
+            //     dial += 100;
+            //     count += 1;
+            // }
+            // if dial == 0 {
+            //     count += 1
+            // }
+            // if dial == 100 {
+            //     dial = 0;
+            //     count += 1;
+            // }
+
+            // this is terrible
+            while rot > 0 {
+                dial += 1;
+                rot -= 1;
+                if dial == 100 {
+                    dial = 0;
+                }
+                if dial == -1 {
+                    dial = 99;
+                }
+                if dial == 0 {
+                    count += 1;
+                }
+            }
+            while rot < 0 {
+                dial -= 1;
+                rot += 1;
+                if dial == 100 {
+                    dial = 0;
+                }
+                if dial == -1 {
+                    dial = 99;
+                }
+                if dial == 0 {
+                    count += 1;
+                }
+            }
+        });
+    count
+}
+
 #[cfg(test)]
 mod tests {
 
-    #[test]
-    fn example() {
-        let input = "L68
+    const INPUT: &str = "L68
 L30
 R48
 L5
@@ -46,7 +107,22 @@ L1
 L99
 R14
 L82";
-        let ans = super::part1(input);
+    #[test]
+    fn example_d1p1() {
+        let ans = super::part1(INPUT);
         assert_eq!(ans, 3)
+    }
+    #[test]
+    fn example_d1p2() {
+        let ans = super::part2(INPUT);
+        assert_eq!(ans, 6)
+    }
+
+    const REAL_INPUT: &str = include_str!("../input/2025/day1.txt");
+
+    #[test]
+    fn real_d1p2() {
+        let ans = super::part2(REAL_INPUT);
+        assert_eq!(ans, 5923)
     }
 }
